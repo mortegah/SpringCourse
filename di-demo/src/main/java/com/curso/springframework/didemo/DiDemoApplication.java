9e408e28c0e8bec39db82cfe20fa298029c04f8a
mortegah <mortegah83@gmail.com>
2018-02-15 19:59:48 +0100

    Hasta cap 31

---
+++ di-demo/src/main/java/com/curso/springframework/didemo/DiDemoApplication.java
@@ -0,0 +1,16 @@
+package com.curso.springframework.didemo;
+
+import com.curso.springframework.didemo.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+		MyController controller = (MyController) ctx.getBean("myController");
+		controller.hello();
+	}
+}
