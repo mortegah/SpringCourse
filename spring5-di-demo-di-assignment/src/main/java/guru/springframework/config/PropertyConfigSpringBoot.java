532557acc006098726260be509177a37879ab9ed
mortegah <mortegah83@gmail.com>
2018-02-27 19:52:17 +0100

    finalizado lecture 6

--- spring5-di-demo-di-assignment/src/main/java/guru/springframework/config/PropertyConfigSpringBoot.java
+++ spring5-di-demo-di-assignment/src/main/java/guru/springframework/config/PropertyConfigSpringBoot.java
@@ -2,25 +2,13 @@ package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
 import guru.springframework.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
-//@Configuration
-////@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-//@PropertySources({
-//        @PropertySource("classpath:datasource.properties"),
-//        @PropertySource("classpath:jms.properties")
-//})
-public class PropertyConfig {
-
-    @Autowired
-    Environment env;
+@Configuration
+public class PropertyConfigSpringBoot {
 
     @Value("${db.username}")
     String user;
@@ -39,7 +27,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(env.getProperty("USERNAME"));
+        fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
@@ -48,8 +36,8 @@ public class PropertyConfig {
     @Bean
     public FakeJmsBroker fakeJmsBroker(){
         FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
-        fakeJmsBroker.setUser(jmsUserName);
         fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setUser(jmsUserName);
         fakeJmsBroker.setUrl(jmsUrl);
         return fakeJmsBroker;
     }
